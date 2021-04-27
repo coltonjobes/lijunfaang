b794bfa36dd35e0d04af3f7b380f3acaef7111f3
Colton Jobes <cjobes@cs.washington.edu>
2021-04-26 22:04:42 -0700

    Initial commit

---
+++ Products.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+/* check out these products! */
+public class Products { /* a good class, but not as good as cse391 */
+    public Product joshsProduct() {
+        return new Product("Dirt", "The good stuff", "dirt.jpeg", 0.0);
+    }
+}
+
---
+++ Staff.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Staff {
+ 
+    public Employee faangPup() {
+        return new Employee("Steve", "Company Pup", "Provides Moral Support", "pup.jpg");
+    }
+
+    public Employee cjobes() {
+		return new Employee("Colton", "Lead Naps Analyst", "What do you mean I'm not valuable to the company?", "colton.jpg");
+    }
+}	
