c592cffc5bc21d2886e83bd64605aab7eb72fa8f
Ryan W Maas <maas@cs.washington.edu>
2021-07-13 18:40:31 +0000

    Make price of dirt 10000

--- Products.java
+++ Products.java
@@ -3,7 +3,7 @@ import java.io.*;
 /* check out these products! */
 public class Products { /* a good class, but not as good as cse391 */
     public Product joshsProduct() {
-        return new Product("Dirt", "The good stuff", "dirt.jpeg", 10.0);
+        return new Product("Dirt", "The good stuff", "dirt.jpeg", 10000.0);
     }
 }
 
