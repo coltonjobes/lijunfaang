51f2f91fd047ef818a7baac1c81df356879b6c31
Anna Batra <batraa@uw.edu>
2021-07-20 13:39:36 -0700

    Add batraa as staff member and upload product

--- Products.java
+++ Products.java
@@ -5,5 +5,9 @@ public class Products { /* a good class, but not as good as cse391 */
     public Product joshsProduct() {
         return new Product("Dirt", "The good stuff", "dirt.jpeg", 10000.0);
     }
+
+    public Product batraaProduct(){
+        return new Product("Sunglasses", "Purple, put on your dog for best return on cute happiness. Dog not for sale.", "batraa_sunglasses.jpeg", 999.99);
+    }
 }
 
--- Staff.java
+++ Staff.java
@@ -9,4 +9,8 @@ public class Staff {
     public Employee cjobes() {
 		return new Employee("Colton", "Lead Naps Analyst", "What do you mean I'm not valuable to the company?", "colton.jpg");
     }
+
+    public Employee batraa(){
+        return new Employee("Anna", "Junior git learner", "Hello, I look forward to working with you all.", "batraa.jpeg");
+    }
 }	
