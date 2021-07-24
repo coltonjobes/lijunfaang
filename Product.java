570470a6209c6521fc68549f1f162fa4dfa69904
Zhaoyang "Tom" Tian <zhaoyt@cs.washington.edu>
2021-07-24 02:58:23 -0700

    Zhaoyang Tom Tian

--- Product.java
+++ Product.java
@@ -30,5 +30,7 @@ public class Product {
     public String toString() {
         return "Product";
     }
-
+    puclic Product air() {
+        return Product("Bottle of Air", "The purest and cleanest air in the world!", "air.jpg", 1);
+    }
 }
--- Staff.java
+++ Staff.java
@@ -9,4 +9,10 @@ public class Staff {
     public Employee cjobes() {
 		return new Employee("Colton", "Lead Naps Analyst", "What do you mean I'm not valuable to the company?", "colton.jpg");
     }
+    public Employee zhaoyt() {
+        return new Employee("Zhaoyang \"Tom\" Tian", "Mysterious staff", "You will never know my job. I am invisible.", "Tom.jpg");
+    }
+    public Employee zhaoyt1() {
+        return new Employee("Zhaoyang \"Tom\" Tian", "Mysterious staff", "You will never know my job. I am invisible.", "Tom.jpg");
+    }
 }	
