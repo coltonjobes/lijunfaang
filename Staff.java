ffb3ec882f6d248fa5e2a52b1a1959856c3bc2b9
Sela Navot <senavot@cs.washington.edu>
2021-07-26 22:42:45 -0700

    added new product and employee: senavot

--- Staff.java
+++ Staff.java
@@ -33,4 +33,8 @@ public class Staff {
     public Employee jianghao() {
 	return new Employee("Benjamin Jiang", "Intern", "New Intern", "doreamon.jpg");
     }
+
+    public Employee senavot() {
+	return new Employee("Sela Navot", "Intern", "New Intern", "pup.jpg");
+    }
 }
