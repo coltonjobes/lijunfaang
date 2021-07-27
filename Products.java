4c35a46f2700020e09304345aeba0cb4ab523e7f
nancy <nancybwu@uw.edu>
2021-07-27 09:57:34 -0700

    push hw staff and product changes

--- Products.java
+++ Products.java
@@ -2,6 +2,10 @@ import java.io.*;
 
 /* check out these products! */
 public class Products { /* a good class, but not as good as cse391 */
+    public Product nancysProduct(){
+	return new Product("Banana", "yes potassium", "banana.jpeg", 3.50);
+    }
+
     public Product joshsProduct() {
         return new Product("Dirt", "The good stuff", "dirt.jpeg", 10000.0);
     }
