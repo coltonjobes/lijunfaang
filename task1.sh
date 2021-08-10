94507290ad71799adb437880bd9bc395165391c7
akshat07 <akshat07@cs.washington.edu>
2021-08-10 05:21:56 -0700

    Probelm 1&2

--- task1.sh
+++ task1.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 ####################################
-# Name: <Your name here>
+# Name: Akshat Mardia
 # CSE 391 - Winter 2021
 # Homework 7 - Task 1
 ####################################
 
 function problem1 {
   # Type your answer to problem #1 below this line
-  echo "not yet implemented"
+  sed -i 's/Insert Catchy Slogan Here/Happiest Place on Earth/' GenerateSite.java
 }
 
 function problem2 {
   # Type your answer to problem #2 below this line
-  echo "not yet implemented"
+  sed -i 's/\<dirt\>/soil/Ig' Products.java
 }
 
 function problem3 {
